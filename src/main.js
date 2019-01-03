c838945d085e30149e8b4db7d1aaac79d6287575
Inzam Zaman <inzam.zaman@inzam.com>
2019-01-03 17:12:17 +0600

    custom directictive

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+//custom directives
+Vue.directive('rainbow',{
+  bind(el, binding, vnode){
+    el.style.color = '#'+Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme',{
+  bind(el, binding, vnode){
+    if(binding.value == 'wide'){
+      el.style.maxWidth = "1200px";
+    }
+    if(binding.arg == 'column'){
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+})
 
 //Vue.config.productionTip = false
 
