19e2d64a344ac53dc94c4ff4b5a3614f579d4bb9
Inzam Zaman <inzam.zaman@inzam.com>
2019-01-04 18:38:24 +0600

    route parameters

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import showBlogs from './components/showBlogs.vue';
 import addBlog from './components/addBlog.vue';
 import listBlogs from './components/listBlogs.vue';
+import singleBlog from './components/singleBlog.vue';
 
 export default[
     {path:'/',component:showBlogs},
     {path:'/add',component:addBlog},
-    {path:'/list',component:listBlogs}
+    {path:'/list',component:listBlogs},
+    {path:'/blog/:id',component:singleBlog}
 ]
     
