75bef886aee469fb6a3655e4158e2e366374cdba
Inzam Zaman <inzam.zaman@inzam.com>
2019-01-04 15:22:53 +0600

    mixins

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div v-theme:column="'wide'" id="show-blogs">
-    <h1>All Blog Article</h1>
+    <h1>List of Blogs</h1>
     <input type="text" v-model="search" placeholder="search blogs">
     <div v-for="blog in filteredBlogs" class="single-blog">
-        <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
-        <article>{{blog.body|snippet}}</article>
+        <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>        
     </div>
   </div>
 </template>
