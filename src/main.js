1a8d1779c9dd1c9a228865cc49a708cf85c30082
Inzam Zaman <inzam.zaman@inzam.com>
2019-01-04 16:07:24 +0600

    setting up router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes.js'
+
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+});
 
 //custom directives
 // Vue.directive('rainbow',{
@@ -37,5 +45,6 @@ Vue.filter('snippet',function(value){
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router: router
 })
